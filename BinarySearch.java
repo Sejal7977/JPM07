3a64405dab34294eed5a48658f0342c264274294
Sejal7977 <151631561+Sejal7977@users.noreply.github.com>
2023-12-22 12:07:56 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,31 @@
+package org.umit.jpm07;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int x = 5;
+        int result = binarySearch(arr, x);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
